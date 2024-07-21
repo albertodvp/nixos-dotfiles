593a29db71263a12ba8122dadc95e8dcc2119c07
Alberto Fanton <alberto.fanton@protonmail.com>
2024-07-21 12:23:58 +0200

    Fix hyprland screen share

---
+++ modules/cachix/sc-tools.nix
@@ -0,0 +1,13 @@
+{
+  nix = {
+    settings = {
+
+      substituters = [
+        "https://sc-tools.cachix.org"
+      ];
+      trusted-public-keys = [
+        "sc-tools.cachix.org-1:DY2+6v0HuMvoCt7wEqZTPqzZBcNk/Lexb72Vixz6n6I="
+      ];
+    };
+  };
+}
--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,7 +1,22 @@
-{ home-manager, ... }: {
+{ home-manager, pkgs, ... }: {
   imports = [
     home-manager.nixosModules.home-manager
   ];
+  programs.hyprland.enable = true;
+  xdg = {
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-hyprland
+      ];
+      config.common.default = "*";
+    };
+  };
+
+  environment.sessionVariables = {
+    GTK_USE_PORTAL = "1";
+  };
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
