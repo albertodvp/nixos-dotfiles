937cf3358e7334ccdb3a834fa0ca3b722c3ba4ea
Alberto Fanton <alberto.fanton@protonmail.com>
2024-08-24 09:02:20 +0200

    Update obs

--- modules/home/obs-studio.nix
+++ modules/home/obs-studio.nix
@@ -2,7 +2,7 @@
 {
   programs.obs-studio = {
     enable = true;
-    plugins = with pkgs.obs-studio-plugins; [ wlrobs input-overlay ];
+    plugins = with pkgs.obs-studio-plugins; [ input-overlay ];
 
   };
 }
